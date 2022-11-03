7912c5583eabd1f9909f665427830b3540c878be
Jing Peng <qiqiai77a@126.com>
2022-11-02 22:49:42 -0400

    finish the frame of each tab

--- src/Games/Games.js
+++ src/Games/Games.js
@@ -25,40 +25,9 @@ export default function Games() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Album layout
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
-          </Container>
-        </Box>
+        <Typography gutterBottom variant="h5" component="h2">
+                      This should be Games page
+         </Typography>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
@@ -95,21 +64,6 @@ export default function Games() {
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-      </Box>
-      {/* End footer */}
     </ThemeProvider>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,15 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-
-// internal components
-import Home from '../Home/Home';
-import Teams from '../Teams/Teams';
-import Players from '../Players/Players';
-import Games from '../Games/Games';
 
 function Header(props) {
   const { sections, title } = props;
@@ -18,7 +10,6 @@ function Header(props) {
   return (
     <React.Fragment>
       <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <img src="../pics/nba_icon.png" alt="logo" />
         <Button size="small">Subscribe</Button>
         <Typography
           component="h2"
@@ -39,19 +30,6 @@ function Header(props) {
         variant="dense"
         sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
       >
-        {/* <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="secondary"
-          textColor="inherit"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Home" {...Home} />
-          <Tab label="Players" {...Players} />
-          <Tab label="Teams" {...Teams} />
-          <Tab label="Games" {...Games} />
-        </Tabs> */}
       </Toolbar>
     </React.Fragment>
   );
