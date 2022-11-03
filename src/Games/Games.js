73b783d60ba80b4d2393dde952e176a6cf0588ac
Jing Peng <qiqiai77a@126.com>
2022-11-03 10:52:27 -0400

    Modified games page by adding team vs team cards

--- src/Games/Games.js
+++ src/Games/Games.js
@@ -1,63 +1,108 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import IconButton from '@mui/material/IconButton';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
+// styles
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+
+// internal component
+import LeaderPlayers from "./LeaderPlayers";
+
+// date type
+import dayjs, { Dayjs } from 'dayjs';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
+// const [value, setValue] = React.useState<Dayjs | null>(
+//     dayjs('2014-08-18T21:11:54')
+//   );
+
+//   const handleChange = (newValue) => {
+//     setValue(newValue);
+//   };
 export default function Games() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <main>
         <Typography gutterBottom variant="h5" component="h2">
-                      This should be Games page
-         </Typography>
+          This should be Games page
+        </Typography>
         <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
+          <Grid container spacing={2}>
+
+            {/* NBA Game date select card */}
+            {/* <Grid item xs={12} >
+                <Card sx={{ display: "flex" }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateTimePicker
+                            renderInput={(props) => <TextField {...props} />}
+                            label="DateTimePicker"
+                        />
+                     </LocalizationProvider>
+                </Card>
+            </Grid> */}
+            {/* NBA Game info card */}
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
+              <Grid item key={card} xs={12} >
+                <Card sx={{ display: "flex" }}>
+
                   <CardMedia
                     component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
+                    sx={{ width: 151 }}
                     image="https://source.unsplash.com/random"
-                    alt="random"
+                    alt="Live from space album cover"
                   />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
+                  <Box sx={{ display: "flex", flexDirection: "column" }}>
+                    <CardContent sx={{ flex: "1 0 auto" }}>
+                      <Typography component="div" variant="h5" align="center">
+                        121 : 111
+                      </Typography>
+                      <Typography
+                        variant="subtitle1"
+                        color="text.secondary"
+                        component="div"
+                        align="center"
+                      >
+                        Wells Fargo Center, Philadelphia, PA
+                      </Typography>
+                    </CardContent>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        pl: 1,
+                        pb: 1,
+                      }}
+                    >
+                      <IconButton aria-label="play/pause">
+                        <SportsBasketballIcon sx={{ height: 38, width: 38 }} />
+                      </IconButton>
+                      <Button size="small">Learn More</Button>
+                    </Box>
+                  </Box>
+                  <CardMedia
+                    component="img"
+                    sx={{ width: 151 }}
+                    image="https://source.unsplash.com/random"
+                    alt="Live from space album cover"
+                    alignItems="right"
+                  />
+                  <LeaderPlayers />
                 </Card>
               </Grid>
             ))}
---
+++ src/Games/LeaderPlayers.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Box from '@mui/material/Box';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+export default function LeaderPlayers() {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <br/>
+      <Typography
+        component="div"
+        color="text.primary"
+        align="center"
+      >
+        GAME LEADERS
+      </Typography>
+      <br/>
+      <Divider />
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar
+              alt="Kristaps Porzingis"
+              src="/static/images/avatar/1.jpg"
+            />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Kristaps Porzingis"
+            secondary={
+              <React.Fragment>
+                {"WAS | #6 |F-C"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="James Hardon" src="/static/images/avatar/2.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="James Hardon"
+            secondary={
+              <React.Fragment>
+                {"PHI | #1 | G"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </List>
+    </Box>
+  );
+}
