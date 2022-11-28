3effaeac637a43c514d01eb87748c3bc8bef19bf
Jing Peng <qiqiai77a@126.com>
2022-11-28 15:37:30 -0500

    add login with google request

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default class UserService {
   // HTTP GET request to
-  async login(date) {
+  async login() {
     console.log("Calling service for users login");
     const config = {
       ...this.Config,
       ...{
         method: "get",
-        url: `http://3.143.232.110:5011/api/games/${date}`,
+        url: `https://ec2-18-219-149-124.us-east-2.compute.amazonaws.com:5011/index`,
       },
     };
     return await axios(config);
